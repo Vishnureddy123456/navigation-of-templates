2d4a5f64e8d9fd34bbbc52082a5729653d509ff0
Vishnureddy123456 <vishnureddylekkala@gmail.com>
2023-06-14 14:56:31 +0530

    first commit

---
+++ app/views.py
@@ -0,0 +1,9 @@
+from django.shortcuts import render
+
+# Create your views here.
+def Home(request):
+    return render(request,'Home.html')
+def Register(request):
+    return render(request,'Register.html')
+def Login(request):
+    return render(request,'Login.html')
